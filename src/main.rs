77763b727ba74afd64a807cd0c1c1894e69f6853
fritshoogland-yugabyte <81778754+fritshoogland-yugabyte@users.noreply.github.com>
2022-01-12 17:35:21 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-use std::alloc::{alloc_zeroed, Layout};
 use std::env;
 use std::process;
 use sysinfo::SystemExt;
 
-fn allocate_memory(chunk_size: usize, total: i32, pointers: &mut Vec<*mut u8>) {
-    unsafe {
-        for _ in 0..total {
-            let layout = Layout::from_size_align(chunk_size,1).unwrap();
-            let pointer = alloc_zeroed(layout);
-            pointers.push(pointer);
-        }
-    }
+fn print_memory() {
+    let system = sysinfo::System::new_all();
+    println!("total memory: {} MB, available memory: {} MB", system.total_memory()/1024, system.available_memory()/1024);
 }
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -21,24 +14,24 @@ fn main() {
     }
     let allocation_amount: i32 = args[1].parse().unwrap();
 
-    let mut system = sysinfo::System::new_all();
-    system.refresh_all();
-
+    let system = sysinfo::System::new_all();
     if allocation_amount < 1 || allocation_amount as u64 > system.available_memory()/1024 {
         eprintln!("Error: amount of memory must be between 1 and {}.", system.available_memory()/1024);
         process::exit(1);
     }
 
-    let mut pointers: Vec<*mut u8> = Vec::new();
-    let chunk_size = 1_048_576;
-    println!("total memory: {} MB, available memory: {} MB, amount to be allocated: {} MB", system.total_memory()/1024, system.available_memory()/1024, allocation_amount);
+    print_memory();
 
     println!("Press enter to allocate.");
     let mut key = String::new();
     let _input = std::io::stdin().read_line(&mut key).unwrap();
 
-    allocate_memory(chunk_size, allocation_amount, &mut pointers);
+    // amount = # as MB * 1024 (->KB) * 1024 (->B) / 8 (u8 -> u64)
+    let amount_as_u64: usize = ((allocation_amount*1024*1024)/8).try_into().unwrap();
+    let mut hog = vec![0u64; amount_as_u64];
+    hog[0..amount_as_u64].fill(0);
 
+    print_memory();
     println!("Press enter to stop and deallocate");
     let mut key = String::new();
     let _input = std::io::stdin().read_line(&mut key).unwrap();
