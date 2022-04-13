58a36608fc14558f012c4b5c422bc2043b566cfe
Frits Hoogland <fhoogland@yugabyte.com>
2022-04-13 14:14:01 +0200

    Revision, added remarks and updated README.

--- README.md
+++ README.md
@@ -6,34 +6,36 @@ The memory is allocated as anonymous memory.
 # usage
 This program takes the following arguments:
 ```
-eatmemory 0.2.0
+eatmemory 0.3.0
 
 USAGE:
     eatmemory [FLAGS] [OPTIONS]
 
 FLAGS:
     -h, --help         Prints help information
-    -m, --memlock      if set call mlock()
-    -q, --query        if set displays memory and swap details, then quit
+    -m, --memlock      if set try to call mlock()
+    -q, --query        if set displays memory and swap details, then quits
     -V, --version      Prints version information
     -v, --verbosity    if set displays memory and swap details plus actions
 
 OPTIONS:
     -a, --alloc-definition <alloc-definition>
-            size of memory (paged in) in MB, megabytes. -1 means equal to size_definition [default: -1]
+            size of memory (paged in) in MB, megabytes. By default, the entire size is allocated
 
     -s, --size-definition <size-definition>      size of allocation in MB, megabytes [default: 0]
 ```
 
 Use `-q` to get an overview of physical and swap allocations.  
-Use `-s` with a size in MB to allocate memory. This is the virtual memory allocation.  
 
-By default, `-a` is set to -1, which means it will follow the size of `-s` and touch the pages to get these paged in.
-It can be set to another value to show or investigate the difference between the virtual allocation and truly paged memory.  
+Use `-s` with a size in MB to allocate memory. It is required to set `-s` to a nonzero value to make eatmemory allocate memory. This is the virtual memory allocation.  
 
-Another option is to use the `-m` switch. This will execute (libc) mlock(). 
-This might not succeed if the linux user has a lower limit set for locked memory than is requested. eatmemory will tell you.
-Locked memory cannot be swapped.
+By default, the value set with `-s` will <u>allocate</u> and <u>write</u> the allocated memory, paging it in, and adding it to the process' resident set size (RSS).
+
+Use `-a` with a size in MB to allocate an amount of memory different than set with `-s`. This can be used to demonstrate the difference between the virtual set size (VSZ) and resident set size (RSS).
+
+Use `-v` to make eatmemory more verbose, and expose memory sizes and tell what it is doing, as well as the pointer to its allocation. By default, it will just do what it's asked.
+
+use `-m` to use (libc) mlock(). 
 
 Any comments, remarks or advise is welcome.
 
@@ -42,5 +44,5 @@ This utility is inspired by the eatmemory.c program by Julio Viera (https://gith
 
 # installation
 In order to run this utility, you must compile it. The compilation require's Rust's Cargo: https://www.rust-lang.org/tools/install (`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`)
-Git clone this repo, cd into it, and run `cargo build`. That compiles the code.
-You can run it via cargo (`cargo run -- <options>`), or use the compiled executable, available in `target/debug`: `target/debug/eatmemory`
+Git clone this repo, cd into it, and run `cargo build --release`. That compiles the code.  
+After it's compiled, you can run it in the following way:`./target/release/eatmemory`.
--- src/main.rs
+++ src/main.rs
@@ -18,66 +18,86 @@ struct Opts {
     #[structopt(short, long, default_value = "0")]
     /// size of allocation in MB, megabytes.
     size_definition: i64,
-    #[structopt(short, long, default_value = "-1")]
-    /// size of memory (paged in) in MB, megabytes. -1 means equal to size_definition.
-    alloc_definition: i64,
+    #[structopt(short, long)]
+    /// size of memory (paged in) in MB, megabytes. By default, the entire size is allocated.
+    alloc_definition: Option<i64>,
 }
 
+// this function shows the memory usage via the crate 'sysinfo'.
 fn print_memory() {
     let system = sysinfo::System::new_all();
-    println!("{:20}: {:9} MB\n{:20}: {:9} MB\n{:20}: {:9} MB\n{:20}: {:9} MB",
+    println!("{:20}: {:9} MB",
+             "available memory", system.available_memory()/1024
+    );
+    println!("{:20}: {:9} MB, {:20}: {:9} MB, {:20}: {:9} MB",
              "total memory", system.total_memory()/1024,
-             "available memory", system.available_memory()/1024,
              "free memory", system.free_memory()/1024,
              "used memory", system.used_memory()/1024
-            );
-    println!("{:20}: {:9} MB\n{:20}: {:9} MB\n{:20}: {:9} MB",
+    );
+    println!("{:20}: {:9} MB, {:20}: {:9} MB, {:20}: {:9} MB",
              "total swap", system.total_swap()/1024,
              "free swap", system.free_swap()/1024,
              "used swap", system.used_swap()/1024
-            );
+    );
 }
 
 fn main() {
+    // obtain CLI options and set variables.
     let options = Opts::from_args();
     let query = options.query as bool;
     let memlock = options.memlock as bool;
     let verbosity = options.verbosity as bool;
     let size_definition = options.size_definition as i64;
-    let allocation_definition = options.alloc_definition as i64;
+    let allocation_definition = match options.alloc_definition {
+        Some(alloc_definition) => alloc_definition,
+        None => size_definition,
+    };
 
+    // the query otpion simply shows the memory usage and stops.
     if query {
         print_memory();
         process::exit(0);
     };
+    // if there is no memory size specified, we can't allocate anything.
+    // therefore, print the usage, and quit.
     if size_definition == 0 {
         let mut app  = Opts::clap();
         app.print_help().unwrap();
         println!();
         process::exit(0);
     }
+    //
     if verbosity { print_memory() }
 
+    // here the allocation starts
     if verbosity { println!("creating vec with size {} MB", size_definition) }
+    // recalculate the given size in MB to bytes (1024*102), and make it a usize (processor pointer sized unsigned integer).
     let size_as_u8: usize = (size_definition*1024*1024).try_into().unwrap();
-    let allocation_as_u8: usize = if allocation_definition == -1 {
-        size_as_u8
-    } else {
-        (allocation_definition*1024*1024).try_into().unwrap()
-    };
+    let alloc_as_u8: usize = (allocation_definition*1024*1024).try_into().unwrap();
+
+    // this is the allocation for size, which does allocate, but not page in.
     let mut hog = vec![0u8; size_as_u8];
+    // in order to be able to use the allocation, we get a pointer to 'hog'.
     let ptr = hog.as_ptr() as *const c_void;
+    //
     if verbosity { println!("pointer to hog: {:?}", ptr) }
 
+    // if memlock is chosen, we perform mlock.
+    // mlock is taken directly from libc, hence unsafe.
+    // this will fail on most linux systems because of the ulimits set.
     if memlock {
         if verbosity { println!("executing mlock") }
         let result = unsafe { mlock(ptr, size_as_u8) };
         if result != 0 { println!("mlock did not succeed, returned: {}. hint: look at limits via ulimit.", result) }
     }
 
-    if verbosity { println!("allocating vec for {} MB (-1 means creation size)", allocation_definition) }
-    hog[0..allocation_as_u8].fill(0);
+    //
+    if verbosity { println!("allocating vec for {} MB", allocation_definition) }
+    // This takes the 'hog' vec, and fills it with zero's.
+    // by doing this, it will page in memory.
+    hog[0..alloc_as_u8].fill(0);
 
+    // after the allocation and paging, print memory again.
     if verbosity { print_memory() }
 
     println!("done. press enter to stop and deallocate");
