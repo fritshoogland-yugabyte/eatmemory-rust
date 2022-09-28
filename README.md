1b6678ce631ea211d712ed893993821d618167ad
Frits Hoogland <fhoogland@yugabyte.com>
2022-09-28 11:05:05 +0200

    Version 0.4.0. Allocation via vec, vec+mlock, malloc or mmap. Allocation and wait to be able to take advantage of overcommit and run out of memory. Step mode to be able investigate all steps.

--- README.md
+++ README.md
@@ -6,36 +6,47 @@ The memory is allocated as anonymous memory.
 # usage
 This program takes the following arguments:
 ```
-eatmemory 0.3.0
+eatmemory 0.4.0
 
 USAGE:
     eatmemory [FLAGS] [OPTIONS]
 
 FLAGS:
     -h, --help         Prints help information
-    -m, --memlock      if set try to call mlock()
-    -q, --query        if set displays memory and swap details, then quits
+    -q, --query        if set displays memory and swap details, then quits.
+    -v, --verbosity    if set displays memory and swap details, and activity details.
+    -a, --alloc_type   specifies allocation type; default: native, other options: native-wait, mlock, malloc, mmap.
+                       native: allocates a vector, and optionally zero's part of the content, paging in memory.
+                       native-wait: allocates a vector, waits 2 seconds, then optionally zero's the content, paging in memory.
+                       mlock: allocates a vector, and optionally mlock()-s part of it.
+                       malloc: performs malloc(), and optionally memset()-s part of the malloc()-ed memory, then free()-s.
+                       mmap: performs mmap(), and optionally memset()-s part of the mmap()-ed memory, then munmap()-s.
+    -i, --init_size_mb size of the allocation in MB (this is the virtual size).
+    -u, --use_size_mb  size of the to be paged in in MB (this is the resident size). Default use_size_mb == init_size_mb.
+    -s, --step         step mode: stop after performing each action, to allow investigation.
     -V, --version      Prints version information
-    -v, --verbosity    if set displays memory and swap details plus actions
-
-OPTIONS:
-    -a, --alloc-definition <alloc-definition>
-            size of memory (paged in) in MB, megabytes. By default, the entire size is allocated
-
-    -s, --size-definition <size-definition>      size of allocation in MB, megabytes [default: 0]
 ```
 
-Use `-q` to get an overview of physical and swap allocations.  
+Use `-q` to get an overview of physical and swap allocations only.  
 
-Use `-s` with a size in MB to allocate memory. It is required to set `-s` to a nonzero value to make eatmemory allocate memory. This is the virtual memory allocation.  
+Use `-i` with a size in MB to allocate memory. It is required to set `-i` to a nonzero value to make eatmemory allocate memory. This is the virtual memory allocation.  
 
-By default, the value set with `-s` will <u>allocate</u> and <u>write</u> the allocated memory, paging it in, and adding it to the process' resident set size (RSS).
+By default, the value of `-i` will set `-u` to the same value if `-u` is not set, making the entire allocation to be set to zero and thus be paged in.
 
-Use `-a` with a size in MB to allocate an amount of memory different than set with `-s`. This can be used to demonstrate the difference between the virtual set size (VSZ) and resident set size (RSS).
+Use `-u` with a size in MB to allocate an amount of memory different and lower than set with `-i`, which can be as low as 0 (to allocate but not touched and thus not pages in).
+This can be used to show the difference between the virtual set size and the resident set size.
 
-Use `-v` to make eatmemory more verbose, and expose memory sizes and tell what it is doing, as well as the pointer to its allocation. By default, it will just do what it's asked.
+Use `-v` to make eatmemory more verbose, which shows what it is doing, as well as the pointer to its allocation. By default, it will just do what it's asked.
 
-use `-m` to use (libc) mlock(). 
+Use `-s` to let eatmemory pause waiting for enter after virtual allocation/creation, touching the memory/set it to zero, and removing the allocation. By default, eatmemory will only pause waiting for enter after creation and setting it to zero.
+
+use `-a` to choose the allocation method:
+
+- native: a Rust vec (vector) is allocated with a single element which is unsigned 8 bits, so the number of elements represents the size in bytes. The use is setting these elements to zero. This is the default.   
+- native-wait: a Rust vec (vector) is allocated with a single element identical to the 'native' method, the difference is the part of setting the elements to zero with the use setting, which is done after 2 seconds. This allows multiple eatmemory processes to take advantage of linux overcommit and allocate more virtual memory than is available, after which the processes set the contents to zero, which leads to allocating this memory, potentially allocating more memory than is available.  
+- mlock: a Rust vec (vector) is allocating with a single element which is unsigned 8 bits. The use setting calls the libc mlock() function for the size of the use setting.
+- malloc: the libc malloc() function is called to allocate memory for the size of init, then libc memset() is used to set use size bytes to zero, and then libc free() is called to deallocate the memory.
+- mmap: the libc mmap() function is called to allocate anonymous memory for the size of init, then libc memset() is used to set use size bytes to zero, and then libc munmap() is called to deallocate the memory.
 
 Any comments, remarks or advise is welcome.
 
@@ -43,6 +54,7 @@ Any comments, remarks or advise is welcome.
 This utility is inspired by the eatmemory.c program by Julio Viera (https://github.com/julman99/eatmemory.git).
 
 # installation
-In order to run this utility, you must compile it. The compilation require's Rust's Cargo: https://www.rust-lang.org/tools/install (`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`)
+In order to run this utility, you must compile it.  
+The compilation require's Rust's Cargo: https://www.rust-lang.org/tools/install (`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`)  
 Git clone this repo, cd into it, and run `cargo build --release`. That compiles the code.  
 After it's compiled, you can run it in the following way:`./target/release/eatmemory`.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate libc;
 use std::process;
 use sysinfo::SystemExt;
-use libc::{c_void, mlock, malloc, memset, free};
+use libc::{c_void, mlock, malloc, memset, free, mmap, munmap, PROT_READ, PROT_WRITE, MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE};
 use structopt::StructOpt;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Debug, StructOpt)]
 struct Opts {
@@ -12,13 +14,16 @@ struct Opts {
     #[structopt(short, long)]
     /// if set displays memory and swap details plus actions.
     verbosity: bool,
+    #[structopt(short, long)]
+    /// if set stops for every action to allow investigation
+    step: bool,
     #[structopt(short, long, default_value = "0")]
-    /// size of allocation in MB, megabytes.
+    /// size of initialisation/creation in MB, megabytes. This is the virtual set size.
     init_size_mb: i64,
     #[structopt(short, long)]
-    /// size of memory (paged in) in MB, megabytes. By default, the entire size is allocated.
+    /// size of used/touched in MB, megabytes. This is the resident set size. By default, the init size is allocated.
     use_size_mb: Option<i64>,
-    #[structopt(short, long, default_value = "native", possible_values(&["malloc", "native", "mlock"]))]
+    #[structopt(short, long, default_value = "native", possible_values(&["native", "native-wait", "mlock", "malloc", "mmap"]))]
     /// type of allocation
     alloc_type: String,
 }
@@ -50,10 +55,7 @@ fn wait_for_enter() {
 fn main() {
     // obtain CLI options and set variables.
     let options = Opts::from_args();
-    //let query = options.query as bool;
-    //let memlock = options.memlock as bool;
-    //let verbosity = options.verbosity as bool;
-    //let size_definition = options.size_definition as i64;
+    // if use_size_mb (-u) is not set, it will be set to the size of init_size_mb (-i)
     let use_size_mb = match options.use_size_mb {
         Some(use_size_mb) => use_size_mb,
         None => options.init_size_mb,
@@ -75,100 +77,128 @@ fn main() {
     //
     if options.verbosity { print_memory() }
 
-    // here the allocation starts
-    //if options.verbosity { println!("creating vec with size {} MB", options.size_definition) }
-    // recalculate the given size in MB to bytes (1024*102), and make it a usize (processor pointer sized unsigned integer).
     let init_size_as_u8: usize = (options.init_size_mb*1024*1024).try_into().unwrap();
     let use_size_as_u8: usize = (use_size_mb*1024*1024).try_into().unwrap();
-    println!("init_size: {}, use_size: {}", &init_size_as_u8, &use_size_as_u8);
+    println!("Initialization size: {}, Used/page in size: {}", &init_size_as_u8, &use_size_as_u8);
+
     match options.alloc_type.as_str() {
+
         "native" => {
-            println!("native mode");
-            let mut hog = vec![0u8; init_size_as_u8];
-            let ptr = hog.as_ptr() as *const c_void;
-            if options.verbosity { println!("1. initialisation. pointer to vec: {:?}", ptr) }
-            wait_for_enter();
-            if options.verbosity { println!("allocating vec for {} MB", init_size_as_u8/1024/1024) }
-            hog[0..use_size_as_u8].fill(0);
+            println!("native mode (creates vector)");
+            // this is a separate scope for the vector, so the last wait can validate the memory being freed
+            {
+                // this is the virtual allocation
+                let mut hog = vec![0u8; init_size_as_u8];
+                let ptr = hog.as_ptr() as *const c_void;
+                if options.verbosity { println!("1. vector created with size {}. pointer to vector: {:?}", &init_size_as_u8, ptr) }
+                if options.verbosity { print_memory() };
+                if options.step { wait_for_enter() };
+                // this is paging in the vector
+                hog[0..use_size_as_u8].fill(0);
+                if options.verbosity { println!("2. vector set to 0 for {} bytes", &use_size_as_u8) }
+                if options.verbosity { print_memory() };
+                wait_for_enter();
+            }
+            if options.verbosity { println!("3. allocation out of scope") };
             if options.verbosity { print_memory() };
-            if options.verbosity { println!("2. page in vec") }
-            wait_for_enter();
+            if options.step { wait_for_enter() };
+
         },
+
+        "native-wait" => {
+            println!("native mode (creates vector) - allocation after 2 seconds");
+            // this is a separate scope for the vector, so the last wait can validate the memory being freed
+            {
+                // this is the virtual allocation
+                let mut hog = vec![0u8; init_size_as_u8];
+                let ptr = hog.as_ptr() as *const c_void;
+                if options.verbosity { println!("1. vector created with size {}. pointer to vector: {:?}", &init_size_as_u8, ptr) }
+                if options.verbosity { print_memory() };
+                if options.step { wait_for_enter() };
+                // sleep
+                sleep(Duration::from_secs(2));
+                // this is paging in the vector
+                hog[0..use_size_as_u8].fill(0);
+                if options.verbosity { println!("2. vector set to 0 for {} bytes", &use_size_as_u8) }
+                if options.verbosity { print_memory() };
+                wait_for_enter();
+            }
+            if options.verbosity { println!("3. allocation out of scope") };
+            if options.verbosity { print_memory() };
+            if options.step { wait_for_enter() };
+
+        },
+
         "mlock" => {
-            println!("mlock mode (uses native mode/vector)");
-            let hog = vec![0u8; init_size_as_u8];
-            let ptr = hog.as_ptr() as *const c_void;
-            if options.verbosity { println!("1. initialisation. pointer to vec: {:?}", ptr) }
-            let result = unsafe { mlock(ptr, init_size_as_u8) };
-            if result != 0 { println!("mlock did not succeed, returned: {}. hint: look at limits via ulimit.", result) }
+            println!("mlock mode (uses native mode/vector for the pointer to the allocation)");
+            // this is a separate scope for the vector, so the last wait can validate the memory being freed
+            {
+                // this is the virtual allocation
+                let hog = vec![0u8; init_size_as_u8];
+                let ptr = hog.as_ptr() as *const c_void;
+                if options.verbosity { println!("1. vector created with size {}. pointer to vector: {:?}", &init_size_as_u8, ptr) }
+                if options.verbosity { print_memory() };
+                if options.step { wait_for_enter() };
+                // the mlock pages in the memory
+                let result = unsafe { mlock(ptr, use_size_as_u8) };
+                if options.verbosity { println!("2. mlock for {} bytes. result: {}", &use_size_as_u8, result) }
+                if options.verbosity { print_memory() };
+                wait_for_enter();
+            }
+            if options.verbosity { println!("3. allocation out of scope") };
             if options.verbosity { print_memory() };
-            if options.verbosity { println!("2. mlock vec") }
-            wait_for_enter();
+            if options.step { wait_for_enter() };
         },
+
         "malloc" => {
             println!("malloc mode");
+            // this is the virtual allocation
             let pointer = unsafe{ malloc(init_size_as_u8) };
             if pointer.is_null() {
                 println!("malloc did not succeed, returned nullpointer.");
-            } else {
-                println!("malloc succeeded, pointer: {:?}", pointer);
+                process::exit(0);
             };
-            if options.verbosity { println!("1. initialisation. pointer returned by malloc {:?}", pointer) }
-            wait_for_enter();
-            let memset_pointer = unsafe { memset(pointer, 1, use_size_as_u8) };
-            if memset_pointer.is_null() {
-                println!("memset did not succeed, returned nullpointer.");
-            } else {
-                println!("memset succeeded, pointer: {:?}", memset_pointer);
-            };
-            if options.verbosity { println!("2. page in. pointer returned by memset {:?}", memset_pointer) }
+            if options.verbosity { println!("1. malloc for {} bytes. pointer returned by malloc: {:?}", &init_size_as_u8, pointer) }
+            if options.verbosity { print_memory() };
+            if options.step { wait_for_enter() };
+            // memset pages in the memory
+            let memset_pointer = unsafe { memset(pointer, 0, use_size_as_u8) };
+            if memset_pointer.is_null() { println!("memset did not succeed, returned nullpointer.") }
+            if options.verbosity { println!("2. memset for {} bytes. pointer returned by memset {:?}", &use_size_as_u8, memset_pointer) }
             if options.verbosity { print_memory() };
             wait_for_enter();
-            println!("calling free");
+            // free releases the memory
             unsafe { free(pointer) };
-        }
-        &_ => unreachable!()
+            if options.verbosity { println!("3. calling free") };
+            if options.verbosity { print_memory() };
+            if options.step { wait_for_enter() };
+        },
+
+        "mmap" => {
+            println!("mmap mode");
+            // this is the virtual allocation
+            let pointer = unsafe{ mmap(std::ptr::null_mut(), init_size_as_u8, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0) };
+            if pointer == MAP_FAILED {
+                println!("mmap did not succeed, returned MAP_FAILED.");
+                process::exit(0);
+            };
+            if options.verbosity { println!("1. mmap for {} bytes. pointer returned by mmap {:?}", &init_size_as_u8, pointer) }
+            if options.verbosity { print_memory() };
+            if options.step { wait_for_enter() };
+            // memset pages in the memory
+            let memset_pointer = unsafe { memset(pointer, 1, use_size_as_u8) };
+            if memset_pointer.is_null() { println!("memset did not succeed, returned nullpointer.") }
+            if options.verbosity { println!("2. memset for {} bytes. pointer returned by memset {:?}", &use_size_as_u8, memset_pointer) }
+            if options.verbosity { print_memory() };
+            wait_for_enter();
+            // the munmap releases the memory
+            let retval = unsafe { munmap(pointer, init_size_as_u8) };
+            if retval < 0 { println!("munmap did not succeed, returned: {}", retval) }
+            if options.verbosity { println!("3. calling munmap") };
+            if options.step { wait_for_enter() };
+        },
+
+        &_ => unreachable!(),
+
     }
-
-    /*
-    // this is the allocation for size, which does allocate, but not page in.
-    let mut hog = vec![0u8; size_as_u8];
-    // in order to be able to use the allocation, we get a pointer to 'hog'.
-    let ptr = hog.as_ptr() as *const c_void;
-    //
-    if options.verbosity { println!("pointer to hog: {:?}", ptr) }
-     */
-
-    /*
-    // if memlock is chosen, we perform mlock.
-    // mlock is taken directly from libc, hence unsafe.
-    // this will fail on most linux systems because of the ulimits set.
-    if options.memlock {
-        if options.verbosity { println!("executing mlock") }
-        let result = unsafe { mlock(ptr, size_as_u8) };
-        if result != 0 { println!("mlock did not succeed, returned: {}. hint: look at limits via ulimit.", result) }
-    }
-
-     */
-
-    /*
-    //
-    if options.verbosity { println!("allocating vec for {} MB", allocation_definition) }
-    // This takes the 'hog' vec, and fills it with zero's.
-    // by doing this, it will page in memory.
-    hog[0..alloc_as_u8].fill(0);
-
-    // after the allocation and paging, print memory again.
-    if options.verbosity { print_memory() }
-     */
-
-    /*
-    println!("done. press enter to stop and deallocate");
-    let mut key = String::new();
-    let _input = std::io::stdin().read_line(&mut key).unwrap();
-     */
-    //wait_for_enter();
-    if options.verbosity { print_memory() }
-    println!("3. freed");
-    wait_for_enter();
 }
